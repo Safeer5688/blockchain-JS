c3a32e6f5273b1ad553d2d72cfff29f08cbe7a85
Safeer Ahmed <safeer.ahmedk98@gmail.com>
2022-04-05 05:32:59 +0500

    test[dev]: add create_transaction tests
    Signed-off-by: Safeer Ahmed <safeer.ahmedk98@gmail.com>

--- dev/test.js
+++ dev/test.js
@@ -3,8 +3,15 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(124325, "NSDTKL3JK45K234J4", "342TRC3443DD23");
-bitcoin.createNewBlock(567567, "SDDTKL3JK45K234J4", "DFGTRC3443DD23");
-bitcoin.createNewBlock(124325, "FGGHKL3JK45K234J4", "3HFGSC3443DD23");
-bitcoin.createNewBlock(124325, "345HKL3JK45K234J4", "JUSSK9SE43DD23");
+
+bitcoin.createNewTransaction(100, "SAFEER3JK45K234J4" , "SHAMIL3JK45K234J4")
+
+bitcoin.createNewBlock(124234, "SJD0934JRSD09SDSD","SD934JHSD2N3SDS")
+
+bitcoin.createNewTransaction(50, "SAFEER3JK45K234J4" , "SHAMIL3JK45K234J4")
+bitcoin.createNewTransaction(300, "SAFEER3JK45K234J4" , "SHAMIL3JK45K234J4")
+bitcoin.createNewTransaction(2000, "SAFEER3JK45K234J4" , "SHAMIL3JK45K234J4")
+
+bitcoin.createNewBlock(374243, "JSDJ98235J2398","KJSDKLF3985NSK3")
 
 console.log(bitcoin);
