52d196fc5b4ee8907bb09f709917ce64c3a92731
Safeer Ahmed <safeer.ahmedk98@gmail.com>
2022-06-03 00:45:39 +0500

    feat: add mine block api
    Signed-off-by: Safeer Ahmed <safeer.ahmedk98@gmail.com>

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const Blockchain = require("./blockchain")
+const uuid = require('uuid/v1')
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const bitcoin = new Blockchain();
+const nodeAddress = uuid().split('-').join('');
 
 //  Get entire Blockchain
 app.get('/blockchain', function (req, res) {
@@ -17,7 +19,7 @@ app.get('/blockchain', function (req, res) {
 // Create new transaction
 app.post('/transaction', function (req, res) {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
-    res.json({note:`Transaction will be added in block ${blockIndex}.`})
+    res.json({ note: `Transaction will be added in block ${blockIndex}.` })
 })
 
 // mine new block
@@ -27,14 +29,21 @@ app.get('/mine', function (req, res) {
     const previousBlockHash = lastBlock['hash'];
     const currentBlockData = {
         transactions: bitcoin.pendingTransactions,
-        index: lastBlock['index']+1
+        index: lastBlock['index'] + 1
     }
 
-    const nonce = bitcoin.proofOfWork(previousBlockHash,currentBlockData)
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-    const newBlock = bitcoin.createNewBlock();
+    bitcoin.createNewTransaction(12.5,"00", nodeAddress);
+
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: " New block mined successfully",
+        block : newBlock
+    })
 })
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000")
 })
