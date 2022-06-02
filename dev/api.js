1324aa0b272f683a03ea5234ffd43cb6343b3669
Safeer Ahmed <safeer.ahmedk98@gmail.com>
2022-06-02 21:11:19 +0500

    feat: add apis to access blockchain
    Signed-off-by: Safeer Ahmed <safeer.ahmedk98@gmail.com>

--- dev/api.js
+++ dev/api.js
@@ -1,10 +1,40 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const Blockchain = require("./blockchain")
 
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const bitcoin = new Blockchain();
+
+//  Get entire Blockchain
 app.get('/blockchain', function (req, res) {
-    res.send("Hey dev")
+    res.json(bitcoin)
+})
+
+// Create new transaction
+app.post('/transaction', function (req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
+    res.json({note:`Transaction will be added in block ${blockIndex}.`})
+})
+
+// mine new block
+app.get('/mine', function (req, res) {
+
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index']+1
+    }
+
+    const nonce = bitcoin.proofOfWork(previousBlockHash,currentBlockData)
+
+    const newBlock = bitcoin.createNewBlock();
 })
 
 app.listen(3000,function(){
-    console.log("Server started")
+    console.log("Server started on port 3000")
 })
